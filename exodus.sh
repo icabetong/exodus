f800c70f84a5b55d666add681f76d3916d4cc9a7
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-08-29 22:12:27 +0800

    Automatic reload of GTK Theme

--- exodus.sh
+++ exodus.sh
@@ -9,6 +9,7 @@ dest="$HOME/.themes/"
 unzip $temp -d $dest
 mv "$HOME/.themes/gtk-master" "$HOME/.themes/Dracula"
 
+sleep 1
 gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
 gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
 
