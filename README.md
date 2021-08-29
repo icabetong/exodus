5bf0b2ff082f33a08b6f87c8a65ed2ac38ec06e2
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-08-29 22:03:56 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Exodus
+
+A simple bash script for automating my personal preferences in freshly installed and new Linux-powered personal computers.
---
+++ exodus.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+theme='https://github.com/dracula/gtk/archive/master.zip'
+temp='/tmp/dracula.zip'
+ 
+wget -O $temp $theme
+
+dest="$HOME/.themes/"
+unzip $temp -d $dest
+mv "$HOME/.themes/gtk-master" "$HOME/.themes/Dracula"
+
+gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+
+exit
